9070bcf1b256106ec99572330a30d29632a0f8a6
Franlo <franlozok@gmail.com>
2025-09-20 00:01:41 -0300

    ENTIDAD RERSERVA

---
+++ src/main/java/com/gimnasio/reservas/model/Reserva.java
@@ -0,0 +1,31 @@
+package com.gimnasio.reservas.model;
+
+import com.gimnasio.reservas.Enums.Estado;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_dni")
+    private Cliente cliente;
+
+    private LocalDateTime fechaHora;
+
+    @Enumerated(EnumType.STRING)
+    private Estado estado;
+
+    //Instructor puede ser null si el cliente es avanzado
+    @ManyToOne
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+}
