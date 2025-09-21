d625a2243b1a588813dddd356af99c7f6b8e212a
Franlo <franlozok@gmail.com>
2025-09-21 04:18:05 -0300

    ClienteService creada (no implementada)

---
+++ src/main/java/com/gimnasio/reservas/service/ClienteService.java
@@ -0,0 +1,17 @@
+package com.gimnasio.reservas.service;
+
+import com.gimnasio.reservas.model.Cliente;
+import com.gimnasio.reservas.Enums.Nivel;
+import com.gimnasio.reservas.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+    
+    }
+}
