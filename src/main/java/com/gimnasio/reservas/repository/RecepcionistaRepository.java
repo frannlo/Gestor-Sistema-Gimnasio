0afb2d5e5d127b9ae4b673ab981cf9bfee5b6dc1
Franlo <franlozok@gmail.com>
2025-09-21 03:14:35 -0300

    Interfaz RecepcionistaRepository implementada

---
+++ src/main/java/com/gimnasio/reservas/repository/RecepcionistaRepository.java
@@ -0,0 +1,7 @@
+package com.gimnasio.reservas.repository;
+
+import com.gimnasio.reservas.model.Recepcionista;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecepcionistaRepository extends JpaRepository<Recepcionista, Long> {
+}
