34f60a0c2db4294150ea1dcc2ff43319e0b02b7e
Franlo <franlozok@gmail.com>
2025-09-26 02:51:11 -0300

    Avance de ReservaService

--- src/main/java/com/gimnasio/reservas/service/ReservaService.java
+++ src/main/java/com/gimnasio/reservas/service/ReservaService.java
@@ -1,5 +1,23 @@
 package com.gimnasio.reservas.service;
 
+import com.gimnasio.reservas.Enums.Estado;
+import com.gimnasio.reservas.Enums.Nivel;
+import com.gimnasio.reservas.model.*;
+import com.gimnasio.reservas.repository.ClienteRepository;
+import com.gimnasio.reservas.repository.InstructorRepository;
+import com.gimnasio.reservas.repository.ReservaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.*;
+
+import java.time.*;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ReservaService {
-    
+    private final ReservaRepository reservaRepository;
+    private final ClienteRepository clienteRepository;
+    private final InstructorRepository instructorRepository;
 }
