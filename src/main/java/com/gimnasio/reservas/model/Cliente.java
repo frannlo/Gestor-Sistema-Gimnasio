8ab08c5bf8536065f0b9fa354ccd744eca2ae82b
Franlo <franlozok@gmail.com>
2025-09-25 23:25:33 -0300

    fix: El atributo dni ahora es de tipo String

--- src/main/java/com/gimnasio/reservas/model/Cliente.java
+++ src/main/java/com/gimnasio/reservas/model/Cliente.java
@@ -2,17 +2,20 @@ package com.gimnasio.reservas.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.Data;
 import com.gimnasio.reservas.Enums.Nivel;
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Cliente {
     @Id
-    private int dni;
+    private String dni;
 
     private String nombre;
     private String apellido;
+    @Column(unique = true)
     private String correo;
     private String telefono;
     private String pinAcceso;
