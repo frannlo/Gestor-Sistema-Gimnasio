1e0d92a82486bef82c52e0bef4caccacfe3857d7
Franlo <franlozok@gmail.com>
2025-09-28 23:16:12 -0300

    fix: Modificamos el tipo de dato que recibe

--- src/main/java/com/gimnasio/reservas/repository/InstructorRepository.java
+++ src/main/java/com/gimnasio/reservas/repository/InstructorRepository.java
@@ -1,9 +1,10 @@
 package com.gimnasio.reservas.repository;
 
 import com.gimnasio.reservas.model.Instructor;
+import com.gimnasio.reservas.Enums.TipoEntrenamiento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface InstructorRepository extends JpaRepository<Instructor, Long> {
-    List<Instructor> findByTipoEntrenamiento(String tipoEntrenamiento);
+public interface InstructorRepository extends JpaRepository<Instructor, String> {
+    List<Instructor> findByTipoEntrenamiento(TipoEntrenamiento tipoEntrenamiento);
 }
